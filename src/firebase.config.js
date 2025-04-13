cd1bd10abf2d6e20a6c747ce4a7a0ad4dd2fa4ef
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-04-13 21:35:05 +0300

    commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log(process.env.FIREBASE_APP_ID);
 const firebaseConfig = {
   apiKey: "AIzaSyDLOUnQ8NuZQRV17-DJF_lTIx3aniXMiWU",
   authDomain: "house-market-place-app-a37fb.firebaseapp.com",
   projectId: "house-market-place-app-a37fb",
   storageBucket: "house-market-place-app-a37fb.firebasestorage.app",
   messagingSenderId: "143198370514",
-  appId: "1:143198370514:web:86816ee4cffb8882c10b0b"
+  appId: "1:143198370514:web:86816ee4cffb8882c10b0b",
 };
 
 // Initialize Firebase
